6f489547ed1a711b41c7d0fcdca4cc93d66d838d
Jaysicz <oj6zo@icloud.com>
2025-09-18 22:06:42 -0600

    dfgdfg
    dfgdfgdfgdf

--- jayisaacaiautomation/api/index.py
+++ jayisaacaiautomation/api/index.py
@@ -1,9 +1,1 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route("/api")
-def hello():
-    return jsonify({"message": "Hello from Flask on Vercel!"})
-
-# Vercel handles serverless execution; no app.run() needed
+from app import app
